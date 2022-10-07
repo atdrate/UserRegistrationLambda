ba98892829083c2e37d9f659fac026112419b056
subodhNagalwade <subodhnagalwade@gmail.com>
2022-10-07 14:57:33 +0530

    [SUBODH]refactor code by lambda to validate user entry

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace UserRegistrationEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option:\n1.Valid first name\n2.Valid last name\n3.Valid Email\n4.Valid Mobile number(+91 7234567890)\n5.Minimum 8 characters\n6.At least 1 upper case\n7.At least 1 numberic value\n8.At least 1 special character\n9.Email sample");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    ValidFirstName.ValidateFirstName();
+                    break;
+                case 2:
+                    ValidLastName.ValidateLastName();
+                    break;
+                case 3:
+                    ValidEmail.ValidateEmail();
+                    break;
+                case 4:
+                    ValidPhone.ValidateMobileNumber();
+                    break;
+                case 5:
+                    Rule1.ValidatePassword();
+                    break;
+                case 6:
+                    Rule2.ValidatePassword();
+                    break;
+                case 7:
+                    Rule3.ValidatePassword();
+                    break;
+                case 8:
+                    Rule4.ValidatePassword();
+                    break;
+                case 9:
+                    ValidEmailSample validate = new ValidEmailSample();
+                    foreach (string mail in validate.GetList())
+                    {
+                        Console.Write(mail + " : ");
+                        validate.PrintResult(validate.ValidateEmail2(mail));
+                    }
+                    break;
+                case 13:
+                    List<ValidUserLambda> listPerson = new List<ValidUserLambda>();
+                    ValidUserLambda.AddRecords(listPerson);
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("Choose a appropriate option");
+                    break;
+            }
+        }
+    }
+}
---
+++ ValidUserLambda.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationEx
+{
+    public class ValidUserLambda
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string Password;
+        public int PhoneNumber;
+        public ValidUserLambda(string firstName, string lastName, string email, string password, int phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            PhoneNumber = phoneNumber;
+        }
+        public static void AddRecords(List<ValidUserLambda> user)
+        {
+            user.Add(new ValidUserLambda("subodh", "nagalwade", "subod@gmail.com", "maha", 1234567890));
+
+            foreach (var person in user.OrderBy(x => x.FirstName))
+            {
+                Console.WriteLine("FirstName     : " + person.FirstName);
+                Console.WriteLine("LastName      : " + person.LastName);
+                Console.WriteLine("Email         : " + person.Email);
+                Console.WriteLine("Password       : " + person.Password);
+                Console.WriteLine("Mobile Number  : " + person.PhoneNumber);
+            }
+        }
+    }
+}
